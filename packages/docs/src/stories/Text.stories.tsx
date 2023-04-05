ee9d28ea3e078be3b94025a16a361d5612647a92
NatanRei <nreischmura@gmail.com>
2023-04-05 18:25:15 -0300

    feat: setando valores default nas stories

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, ab! Nesciunt modi inventore amet! Aperiam, minima veniam. Nemo dolorem',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
